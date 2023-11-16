5f8383b5964bfbe3e4501cbaa41cf14538d796d6
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-16 23:21:45 +1030

    Add customer and accounts repositories

---
+++ accounts/src/main/java/com/neo42/accounts/repository/AccountsRepository.java
@@ -0,0 +1,8 @@
+package com.neo42.accounts.repository;
+
+import com.neo42.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {}
---
+++ accounts/src/main/java/com/neo42/accounts/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.neo42.accounts.repository;
+
+import com.neo42.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
