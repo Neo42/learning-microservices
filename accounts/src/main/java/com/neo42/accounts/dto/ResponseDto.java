bdf71129a59b995b3c2023883f7a5a081c2ad04b
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-16 23:40:51 +1030

    Add data transfer objects

---
+++ accounts/src/main/java/com/neo42/accounts/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package com.neo42.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+  private String statusCode;
+  private String statusMsg;
+}
