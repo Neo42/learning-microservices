8a38511d107a66980f40dcc17f332c434ef6ef64
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-16 23:20:39 +1030

    Add accounts controller

---
+++ accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
@@ -0,0 +1,12 @@
+package com.neo42.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+  @GetMapping
+  public String sayHello() {
+    return "Hello World";
+  }
+}
