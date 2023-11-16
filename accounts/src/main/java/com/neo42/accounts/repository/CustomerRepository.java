f12de909151aa69bddf7fd2b2ba5718ad43d0780
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 01:30:41 +1030

    feat: add account service createAccount

--- accounts/src/main/java/com/neo42/accounts/repository/CustomerRepository.java
+++ accounts/src/main/java/com/neo42/accounts/repository/CustomerRepository.java
@@ -1,8 +1,11 @@
 package com.neo42.accounts.repository;
 
 import com.neo42.accounts.entity.Customer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+  Optional<Customer> findByMobileNumber(String mobileNumber);
+}
--- accounts/src/main/java/com/neo42/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/neo42/accounts/service/impl/AccountsServiceImpl.java
@@ -1,9 +1,16 @@
 package com.neo42.accounts.service.impl;
 
+import com.neo42.accounts.constants.AccountsConstants;
 import com.neo42.accounts.dto.CustomerDto;
+import com.neo42.accounts.entity.Accounts;
+import com.neo42.accounts.entity.Customer;
+import com.neo42.accounts.exception.CustomerAlreadyExistsException;
+import com.neo42.accounts.mapper.CustomerMapper;
 import com.neo42.accounts.repository.AccountsRepository;
 import com.neo42.accounts.repository.CustomerRepository;
 import com.neo42.accounts.service.IAccountsService;
+import java.util.Optional;
+import java.util.Random;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +24,26 @@ public class AccountsServiceImpl implements IAccountsService {
    * @param customerDto - CustomerDto Object
    */
   @Override
-  public void createAccount(CustomerDto customerDto) {}
+  public void createAccount(CustomerDto customerDto) {
+    Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+    Optional<Customer> optionalCustomer =
+        customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+    if (optionalCustomer.isPresent()) {
+      throw new CustomerAlreadyExistsException(
+          "Customer already registered with give mobile number: " + customerDto.getMobileNumber());
+    }
+    Customer savedCustomer = customerRepository.save(customer);
+    accountsRepository.save(createNewAccount(savedCustomer));
+  }
+
+  private Accounts createNewAccount(Customer customer) {
+    Accounts newAccount = new Accounts();
+    newAccount.setCustomerId(customer.getCustomerId());
+    Long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+    newAccount.setAccountNumber(randomAccNumber);
+    newAccount.setAccountType(AccountsConstants.SAVINGS);
+    newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+    return newAccount;
+  }
 }
