54fce4e4a6eedfc8165ca65e2afa22cbf6c3d7b1
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 23:48:40 +1030

    docs: add title & descriptions for swagger docs

--- accounts/src/main/java/com/neo42/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/neo42/accounts/AccountsApplication.java
@@ -1,11 +1,30 @@
 package com.neo42.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Account microservice REST API Documentation",
+            description = "EasyBank Accounts microservice REST API Documentation",
+            version = "v1",
+            contact =
+                @Contact(name = "Hao Jiang", email = "hi@hao.me", url = "https://github.com/neo42"),
+            license =
+                @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")),
+    externalDocs =
+        @ExternalDocumentation(
+            description = "EasyBank Accounts microservice REST API Documentation",
+            url = "https://github.com/Neo42/learning-microservices"))
 public class AccountsApplication {
 
   public static void main(String[] args) {
