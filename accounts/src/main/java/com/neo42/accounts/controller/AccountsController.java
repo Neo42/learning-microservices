88f2ae176d013cd7e7d1fb59c53190f876ebb324
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 17:21:50 +1030

    feat: add fetchAccountDetails in AccountsController

--- accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
@@ -8,10 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -19,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountsController {
   private IAccountsService iAccountsService;
 
-  @PostMapping(path = "/create")
+  @PostMapping("/create")
   public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
     iAccountsService.createAccount(customerDto);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_2O1));
+  }
+
+  @GetMapping("/fetch")
+  public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
+    return ResponseEntity.status(HttpStatus.OK).body(customerDto);
   }
 }
