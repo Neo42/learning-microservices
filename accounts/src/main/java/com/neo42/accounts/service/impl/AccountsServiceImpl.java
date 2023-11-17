fbda7a6bea6a49beb23d37c8e536c5fe18693177
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 17:21:06 +1030

    feat: implement fetchAccount in AccountsServiceImpl

--- accounts/src/main/java/com/neo42/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/neo42/accounts/service/impl/AccountsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.neo42.accounts.service.impl;
 
 import com.neo42.accounts.constants.AccountsConstants;
+import com.neo42.accounts.dto.AccountsDto;
 import com.neo42.accounts.dto.CustomerDto;
 import com.neo42.accounts.entity.Accounts;
 import com.neo42.accounts.entity.Customer;
 import com.neo42.accounts.exception.CustomerAlreadyExistsException;
+import com.neo42.accounts.exception.ResourceNotFoundException;
+import com.neo42.accounts.mapper.AccountsMapper;
 import com.neo42.accounts.mapper.CustomerMapper;
 import com.neo42.accounts.repository.AccountsRepository;
 import com.neo42.accounts.repository.CustomerRepository;
@@ -50,5 +53,28 @@ public class AccountsServiceImpl implements IAccountsService {
     newAccount.setCreatedAt(LocalDateTime.now());
     newAccount.setCreatedBy("Anonymous");
     return newAccount;
+  }
+
+  /**
+   * @param mobileNumber - Input mobile number
+   * @return Accounts Details based on the input mobileNumber
+   */
+  @Override
+  public CustomerDto fetchAccount(String mobileNumber) {
+    Customer customer =
+        customerRepository
+            .findByMobileNumber(mobileNumber)
+            .orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+    Accounts accounts =
+        accountsRepository
+            .findByCustomerId(customer.getCustomerId())
+            .orElseThrow(
+                () ->
+                    new ResourceNotFoundException(
+                        "Account", "customerId", customer.getCustomerId().toString()));
+    CustomerDto customerDto = CustomerMapper.mapToCustomerDto(customer, new CustomerDto());
+    customerDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+    return customerDto;
   }
 }
