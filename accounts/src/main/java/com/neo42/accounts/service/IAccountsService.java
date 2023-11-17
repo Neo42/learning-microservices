157af39c56d280e9195a1b7bb58c1260e9741abc
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 20:07:05 +1030

    feat: add updateAccount in IAccountsService & AccountsServiceImpl

--- accounts/src/main/java/com/neo42/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/neo42/accounts/service/IAccountsService.java
@@ -13,4 +13,10 @@ public interface IAccountsService {
    * @return Accounts Details based on the input mobileNumber
    */
   CustomerDto fetchAccount(String mobileNumber);
+
+  /**
+   * @param customerDto - customerDto Object
+   * @return boolean indicating if the update of Account details is successful or not
+   */
+  boolean updateAccount(CustomerDto customerDto);
 }
