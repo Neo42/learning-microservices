f5443af24fd34c1762e6b96ad7159683c78b45b2
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 21:02:08 +1030

    feat: add deleteAccountDetails for /delete api in AccountsController

--- accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
@@ -4,7 +4,6 @@ import com.neo42.accounts.constants.AccountsConstants;
 import com.neo42.accounts.dto.CustomerDto;
 import com.neo42.accounts.dto.ResponseDto;
 import com.neo42.accounts.service.IAccountsService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
 public class AccountsController {
   private IAccountsService iAccountsService;
 
@@ -33,6 +31,18 @@ public class AccountsController {
   public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
     boolean isUpdated = iAccountsService.updateAccount(customerDto);
     if (!isUpdated) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+    } else {
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+    }
+  }
+
+  @DeleteMapping("/delete")
+  public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+    boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+    if (!isDeleted) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
           .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
     } else {
