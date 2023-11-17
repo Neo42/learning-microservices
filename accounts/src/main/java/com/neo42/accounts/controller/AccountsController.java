d6fc5e0ee65a1670991f30266eaeb8a81f32b960
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-18 00:47:10 +1030

    docs: add summaries & descriptions for API endpoints

--- accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.neo42.accounts.constants.AccountsConstants;
 import com.neo42.accounts.dto.CustomerDto;
 import com.neo42.accounts.dto.ResponseDto;
 import com.neo42.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +17,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+    name = "REST API for Accounts in EazyBank",
+    description = "REST API in EazyBank to create, fetch, update & delete accounts details")
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
@@ -20,6 +27,11 @@ import org.springframework.web.bind.annotation.*;
 public class AccountsController {
   private IAccountsService iAccountsService;
 
+  @Operation(
+      summary = "Create a bank account",
+      description =
+          "Create a bank account with name, email, mobile number & account related information")
+  @ApiResponse(responseCode = "201", description = "HTTP response status: CREATED")
   @PostMapping("/create")
   public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
     iAccountsService.createAccount(customerDto);
@@ -27,6 +39,10 @@ public class AccountsController {
         .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_2O1));
   }
 
+  @Operation(
+      summary = "Fetch details for an bank account",
+      description = "Fetch Customer & Account details based on a mobile number")
+  @ApiResponse(responseCode = "200", description = "HTTP response status: OK")
   @GetMapping("/fetch")
   public ResponseEntity<CustomerDto> fetchAccountDetails(
       @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
@@ -35,6 +51,13 @@ public class AccountsController {
     return ResponseEntity.status(HttpStatus.OK).body(customerDto);
   }
 
+  @Operation(
+      summary = "Update for an bank account",
+      description = "Update Customer & Account details based on an account number")
+  @ApiResponses({
+    @ApiResponse(responseCode = "200", description = "HTTP response status: OK"),
+    @ApiResponse(responseCode = "500", description = "HTTP response status: Internal Server Error"),
+  })
   @PutMapping("/update")
   public ResponseEntity<ResponseDto> updateAccountDetails(
       @Valid @RequestBody CustomerDto customerDto) {
@@ -48,6 +71,13 @@ public class AccountsController {
     }
   }
 
+  @Operation(
+      summary = "Delete an bank account",
+      description = "Delete Customer & Account details based on a mobile number")
+  @ApiResponses({
+    @ApiResponse(responseCode = "200", description = "HTTP response status: OK"),
+    @ApiResponse(responseCode = "500", description = "HTTP response status: Internal Server Error"),
+  })
   @DeleteMapping("/delete")
   public ResponseEntity<ResponseDto> deleteAccountDetails(
       @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
