c1c491b876d8548b3567459b3864313c2b62b3aa
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 21:57:52 +1030

    Revert "feat: add data validation for CustomerDto, AccountsDto & AccountsController"
    This reverts commit e31916a8e8dd97ccf9341b8e22285d5e0c0fc85d.

--- accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/neo42/accounts/controller/AccountsController.java
@@ -4,40 +4,31 @@ import com.neo42.accounts.constants.AccountsConstants;
 import com.neo42.accounts.dto.CustomerDto;
 import com.neo42.accounts.dto.ResponseDto;
 import com.neo42.accounts.service.IAccountsService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
-@Validated
 public class AccountsController {
   private IAccountsService iAccountsService;
 
   @PostMapping("/create")
-  public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+  public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
     iAccountsService.createAccount(customerDto);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_2O1));
   }
 
   @GetMapping("/fetch")
-  public ResponseEntity<CustomerDto> fetchAccountDetails(
-      @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
-          String mobileNumber) {
+  public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
     CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
     return ResponseEntity.status(HttpStatus.OK).body(customerDto);
   }
 
   @PutMapping("/update")
-  public ResponseEntity<ResponseDto> updateAccountDetails(
-      @Valid @RequestBody CustomerDto customerDto) {
+  public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
     boolean isUpdated = iAccountsService.updateAccount(customerDto);
     if (!isUpdated) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -49,9 +40,7 @@ public class AccountsController {
   }
 
   @DeleteMapping("/delete")
-  public ResponseEntity<ResponseDto> deleteAccountDetails(
-      @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
-          String mobileNumber) {
+  public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
     boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
     if (!isDeleted) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
--- accounts/src/main/java/com/neo42/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/neo42/accounts/dto/AccountsDto.java
@@ -1,21 +1,10 @@
 package com.neo42.accounts.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
-
-  @NotEmpty(message = "Email can not be a null or empty")
-  @Pattern(
-      regexp = "(^$|[0-9]{10})",
-      message = "AccountNumber must be 10 digits") // 10 digit number
   private Long accountNumber;
-
-  @NotEmpty(message = "AccountType can not be a null or empty")
   private String accountType;
-
-  @NotEmpty(message = "branchAddress can not be a null or empty")
   private String branchAddress;
 }
