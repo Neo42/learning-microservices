f7d1a4af431bc720f0e744ef06175f2141d77dfb
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 17:10:12 +1030

    feat: add findByCustomerId to AccountsRepository

--- accounts/src/main/java/com/neo42/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/neo42/accounts/repository/AccountsRepository.java
@@ -1,8 +1,11 @@
 package com.neo42.accounts.repository;
 
 import com.neo42.accounts.entity.Accounts;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountsRepository extends JpaRepository<Accounts, Long> {}
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+  Optional<Accounts> findByCustomerId(Long customerId);
+}
