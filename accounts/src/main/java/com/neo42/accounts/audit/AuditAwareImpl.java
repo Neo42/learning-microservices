270bd2cfbb6814a84fb263e76e044226926df703
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-11-17 22:39:51 +1030

    feat: add JPA auditing for entity creation & update

---
+++ accounts/src/main/java/com/neo42/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,13 @@
+package com.neo42.accounts.audit;
+
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    return Optional.of("ACCOUNTS_MS");
+  }
+}
